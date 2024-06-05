0505b9af48b5460222aecffc10139fc4777b33ca
zxcd <228587199@qq.com>
2024-06-05 14:28:14 +0800

    【test】add cli test readme (#3784)
    * add cli test readme
    
    * fix code style

--- tests/unit/cli/test_cli.sh
+++ tests/unit/cli/test_cli.sh
@@ -110,5 +110,7 @@ paddlespeech whisper --task transcribe --input ./zh.wav
 # whisper recognize text and translate to English
 paddlespeech whisper --task translate --input ./zh.wav
 
+# to change model English-Only model
+paddlespeech whisper --lang en --size base --task transcribe  --input ./en.wav
 
 echo -e "\033[32mTest success !!!\033[0m"
