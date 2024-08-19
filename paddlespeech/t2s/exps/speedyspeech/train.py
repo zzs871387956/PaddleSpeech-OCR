dce786340487ecfd591f5aa4637796990bd37f26
warrentdrew <yipinzhu@outlook.com>
2024-08-19 17:10:51 +0800

    fix help message

--- paddlespeech/t2s/exps/speedyspeech/train.py
+++ paddlespeech/t2s/exps/speedyspeech/train.py
@@ -194,17 +194,19 @@ def main():
         "--nxpu",
         type=int,
         default=0,
-        help="if nxpu == 0 and ngpu == 0 and nnpu == 0, use cpu.")
+        help="if wish to use xpu, set ngpu == 0 and nxpu > 0, otherwise use gpu, npu or cpu."
+    )
     parser.add_argument(
         "--nnpu",
         type=int,
         default=0,
-        help="if nnpu == 0 and nxpu == 0 and ngpu == 0, use cpu.")
+        help="if wish to use npu, set ngpu == 0 and nnpu > 0, otherwise use gpu, xpu or cpu."
+    )
     parser.add_argument(
         "--ngpu",
         type=int,
         default=1,
-        help="if ngpu == 0, use cpu or xpu or npu")
+        help="if wish to use gpu, set ngpu > 0, otherwise use xpu, npu or cpu.")
 
     parser.add_argument(
         "--use-relative-path",
