0871d0a26cebb96a209b5f6ea68ab3c06b3ab23a
warrentdrew <yipinzhu@outlook.com>
2024-08-20 18:24:49 +0800

    speedyspeech code adapt for mlu

--- paddlespeech/t2s/exps/inference.py
+++ paddlespeech/t2s/exps/inference.py
@@ -112,7 +112,7 @@ def parse_args():
     parser.add_argument(
         "--device",
         default="gpu",
-        choices=["gpu", "cpu", "xpu", "npu"],
+        choices=["gpu", "cpu", "xpu", "npu", "mlu"],
         help="Device selected for inference.", )
     parser.add_argument('--cpu_threads', type=int, default=1)
 
