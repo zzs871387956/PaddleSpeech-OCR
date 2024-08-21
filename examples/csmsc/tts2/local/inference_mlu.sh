143fe66d7b2276ba7d285ad7933a32ce33d144a7
warrentdrew <yipinzhu@outlook.com>
2024-08-21 19:28:35 +0800

    fix inference

--- examples/csmsc/tts2/local/inference_mlu.sh
+++ examples/csmsc/tts2/local/inference_mlu.sh
@@ -5,22 +5,9 @@ train_output_path=$1
 stage=0
 stop_stage=0
 
-# pwgan
-if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-    python3 ${BIN_DIR}/../inference.py \
-        --inference_dir=${train_output_path}/inference \
-        --am=speedyspeech_csmsc \
-        --voc=pwgan_csmsc \
-        --text=${BIN_DIR}/../../assets/sentences.txt \
-        --output_dir=${train_output_path}/pd_infer_out \
-        --phones_dict=dump/phone_id_map.txt \
-        --tones_dict=dump/tone_id_map.txt \
-        --device mlu
-fi
-
 # for more GAN Vocoders
 # multi band melgan
-if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     python3 ${BIN_DIR}/../inference.py \
         --inference_dir=${train_output_path}/inference \
         --am=speedyspeech_csmsc \
@@ -33,7 +20,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 fi
 
 # hifigan
-if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     python3 ${BIN_DIR}/../inference.py \
         --inference_dir=${train_output_path}/inference \
         --am=speedyspeech_csmsc \
